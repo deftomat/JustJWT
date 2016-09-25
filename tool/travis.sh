b9d1dd1243de7f9c89ec90061b41f8fee2ffbbdf
Tomas Szabo <tomas.szabo@deftomat.com>
2016-09-25 14:25:22 +0200

    Removed coveralls

--- tool/travis.sh
+++ tool/travis.sh
@@ -5,12 +5,3 @@ set -e
 
 # Run the tests.
 pub run test -p vm,content-shell
-
-# Install dart_coveralls; gather and send coverage data.
-if [ "$COVERALLS_TOKEN" ] && [ "$TRAVIS_DART_VERSION" = "stable" ]; then
-  pub global activate dart_coveralls
-  pub global run dart_coveralls report \
-    --retry 2 \
-    --exclude-test-files \
-    test/test_all.dart
-fi
