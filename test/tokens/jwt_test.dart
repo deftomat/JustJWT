4497f10384d647fbc5981bba9413d9ce5e914c0a
Tomas Szabo <tomas.szabo@deftomat.com>
2016-09-25 16:36:01 +0200

    Polished tests

--- test/tokens/jwt_test.dart
+++ test/tokens/jwt_test.dart
@@ -3,11 +3,11 @@ import 'package:just_jwt/src/tokens.dart';
 
 void main() {
   group('The HS256 JWT ', () {
-    testJwt((payload) => new Jwt.HS256(payload), 'HS256');
+    testJwt((Map payload) => new Jwt.HS256(payload), 'HS256');
   });
 
   group('The RS256 JWT ', () {
-    testJwt((payload) => new Jwt.RS256(payload), 'RS256');
+    testJwt((Map payload) => new Jwt.RS256(payload), 'RS256');
   });
 }
 
